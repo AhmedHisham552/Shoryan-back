12454983fd1bbdec45bef8af785d5c68c0164e2f
Youssef Walid <yousefwalid98@hotmail.com>
2019-12-15 16:15:47 +0200

    Added StoredProcedures Folder

---
+++ Shoryan/Controllers/GiftCardsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DBapplication;
+
+namespace Shoryan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftCardsController : ControllerBase
+    {
+		DBManager dbMan;
+
+		public GiftCardsController()
+		{
+			dbMan = new DBManager();
+		}
+
+		public void addGiftCard(string code, int value, DateTime expiryDate)
+        {
+            string StoredProcedureName = GiftCardsProcedures.addGiftCard;
+            Dictionary<string, object> Parameters = new Dictionary<string, object>();
+            Parameters.Add("@code", code);
+            Parameters.Add("@value", value);
+            Parameters.Add("@expiryDate", expiryDate);
+            dbMan.ExecuteNonQuery(StoredProcedureName, Parameters);
+        }
+    }
+}
